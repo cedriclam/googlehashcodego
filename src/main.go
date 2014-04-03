860ddfd1ec2b64c6eb3a52b6b8b82776132ad526
Cedric Lamoriniere <cedric.lamoriniere@gmail.com>
2014-04-03 23:31:34 +0200

    add stdin reader

--- src/main.go
+++ src/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"log"
+	"io/ioutil"
+	"bufio"
+	"container/list"
 	"github.com/cedriclam/googlehashcodego/src/utils"
 )
 
@@ -17,7 +22,40 @@ func main() {
 	if *versionFlag {
 		fmt.Println("Version:", APP_VERSION)
 	}
+	bio := bufio.NewReader(os.Stdin)
+	
+	var line []byte
+	hasMoreInLine := true
+	var err error = nil
 
+	for hasMoreInLine  {
+		line, hasMoreInLine, err = bio.ReadLine()
+		if err == nil {
+			fmt.Println(line)
+		}
+	}
+		
 	utils.GetInput("foo")
 
+}
+
+func ReadFromStdinToBytes() ([]byte, error){
+	bytes, err := ioutil.ReadAll(os.Stdin)
+	return bytes, err;
+}
+
+func ReadFromStdinToLines(iList *list.List) {
+	reader := bufio.NewReader(os.Stdin)
+
+    for {
+        line, err := reader.ReadString('\n')
+
+        if err != nil {
+            // You may check here if err == io.EOF
+            break
+        }
+
+		iList.PushBack(line)
+        log.Println(line)
+    }
 }
